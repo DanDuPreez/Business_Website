346fe83db88451cbaf6c8bad532590e684e3f040
Daniel du Preez <dandupreez@legiondev.co.za>
2023-12-29 10:59:29 +0200

    Change to match student solution

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(layout='wide')
 
-st.title('The Best Company')
+st.header('The Best Company')
 
 content = '''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
@@ -13,26 +13,26 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
 '''
 st.write(content)
 
-st.header('Our Team')
+st.subheader('Our Team')
 
 df = pd.read_csv('data.csv')
 
-col1, empty_col1, col2, empty_col2, col3 = st.columns([1.5, 1.5, 1.5, 1.5, 1.5])
+col1, col2, col3 = st.columns(3)
 
 with col1:
     for index, row in df[:4].iterrows():
-        st.header(row['first name'] + ' ' + row['last name'])
+        st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row['role'])
         st.image('images/' + row['image'])
 
 with col2:
     for index, row in df[4:8].iterrows():
-        st.header(row['first name'] + ' ' + row['last name'])
+        st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row['role'])
         st.image('images/' + row['image'])
 
 with col3:
     for index, row in df[8:].iterrows():
-        st.header(row['first name'] + ' ' + row['last name'])
+        st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row['role'])
         st.image('images/' + row['image'])
