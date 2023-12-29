33d04798284ea87d67c78e268d234ab08f43da93
Daniel du Preez <dandupreez@legiondev.co.za>
2023-12-29 10:49:50 +0200

    Create business website. Added Team, descriptions, images and roles.

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
 import streamlit as st
 import pandas as pd
+
+st.set_page_config(layout='wide')
+
+st.title('The Best Company')
+
+content = '''
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+'''
+st.write(content)
+
+st.header('Our Team')
+
+df = pd.read_csv('data.csv')
+
+col1, empty_col1, col2, empty_col2, col3 = st.columns([1.5, 1.5, 1.5, 1.5, 1.5])
+
+with col1:
+    for index, row in df[:4].iterrows():
+        st.header(row['first name'] + ' ' + row['last name'])
+        st.write(row['role'])
+        st.image('images/' + row['image'])
+
+with col2:
+    for index, row in df[4:8].iterrows():
+        st.header(row['first name'] + ' ' + row['last name'])
+        st.write(row['role'])
+        st.image('images/' + row['image'])
+
+with col3:
+    for index, row in df[8:].iterrows():
+        st.header(row['first name'] + ' ' + row['last name'])
+        st.write(row['role'])
+        st.image('images/' + row['image'])
