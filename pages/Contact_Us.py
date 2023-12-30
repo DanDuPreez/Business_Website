67477616fafd3ec146078238878d0267416d059e
Daniel du Preez <dandupreez@legiondev.co.za>
2023-12-30 15:12:03 +0200

    Edit to match student solution

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,16 +1,20 @@
 import streamlit as st
 from send_email import send_email
+import pandas
 
-with st.form(key='form'):
-    email = st.text_input('Your Email Address')
-    topic = st.selectbox('What topic do you want to discuss?', ['Job Inquiries', 'Project Proposals', 'Other'])
+df = pandas.read_csv('topics.csv')
+
+with st.form(key='email_form'):
+    user_email = st.text_input('Your Email Address')
+    option = st.selectbox('What topic do you want to discuss?', df['topic'])
     raw_message = st.text_area('Text')
-    message = f'''\
-    Subject : {topic} from {email}
+    message = f"""\
+Subject : New email from {user_email}
     
-    From: {email}
-    {raw_message}
-    '''
+From: {user_email}
+Topic {option}
+{raw_message}
+"""
     button = st.form_submit_button('Submit')
     if button:
         send_email(message)
